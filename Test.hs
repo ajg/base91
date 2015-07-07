ee9125cbbf2a39b530661904577196bc3cc8029d
Alvaro J. Genial <genial@alva.ro>
2015-07-07 17:38:58 -0400

    Add instance tests

--- Test.hs
+++ Test.hs
@@ -3,7 +3,13 @@
 import Test.QuickCheck
 import Codec.Binary.Base91.String (decode, encode)
 
-main = quickCheck prop_identity where
-  prop_identity ws = decode (encode ws) == ws
-  -- Note that the reverse identity, encode (decode cs) == cs, isn't true because not every
-  -- arbitrary string is valid Base91, even if each character is constrained to the Base91 alphabet.
+main = do
+    quickCheck $ prop_identity
+    quickCheck $ prop_instance ([], [])
+    quickCheck $ prop_instance ([72,101,108,108,111,44,32,119,111,114,108,100,33], ">OwJh>}A\"=r@@Y?F")
+  where
+    prop_identity ws = decode (encode ws) == ws
+    -- Note that the reverse identity, encode (decode cs) == cs, isn't true because not every
+    -- arbitrary string is valid Base91, even if each character is constrained to the Base91 alphabet.
+    prop_instance (ws, cs) = encode ws == cs
+                          && decode cs == ws
