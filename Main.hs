6ba5315ff76808ec7e72adb3e8d2f92647ae81e0
Alvaro J. Genial <genial@alva.ro>
2015-07-09 17:11:59 -0400

    Extend utility options

--- Main.hs
+++ Main.hs
@@ -5,10 +5,18 @@ module Main (main) where
 import Codec.Binary.Base91.Efficient as Base91
 import Data.ByteString as BS
 import Data.Text.IO as T
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 
 main :: IO ()
-main = getArgs >>= \args -> case args of
-  ["encode"] -> BS.getContents >>= T.putStrLn . Base91.encode
-  ["decode"] -> T.getContents >>= BS.putStr . Base91.decode
-  _          -> error "invalid arguments"
+main = getProgName >>= \name -> getArgs >>= \args -> case name:args of
+    "b91enc":[]    -> encode
+    "b91dec":[]    -> decode
+    _:["-e"]       -> encode
+    _:["-d"]       -> decode
+    _:["--encode"] -> encode
+    _:["--decode"] -> decode
+    _              -> error "missing or invalid arguments"
+
+  where
+    encode  = BS.getContents >>= T.putStrLn . Base91.encode
+    decode  = T.getContents >>= BS.putStr . Base91.decode
